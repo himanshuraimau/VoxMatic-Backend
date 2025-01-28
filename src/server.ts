91630738296ed399313ff327af32c2f3103faee9
Himanshu Rai <himanshuraimau9@gmail.com>
2025-01-28 20:31:21 +0530

    add zod dependency and update server log message; configure nodemon for TypeScript

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,13 @@ import app from './app';
 import connectDB from './database';
 
 const PORT = process.env.PORT || 3000;
+
+// Connect to Database
 connectDB();
 
+// Start server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 
